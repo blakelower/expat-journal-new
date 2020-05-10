a6c642e90868759205f097235f1c912e6f6e5116
blakelower <blakelower1999@gmail.com>
2020-05-10 12:27:06 -0700

    added image with text at bottom

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,29 @@ import React from "react";
 import "./styles/nav.css";
 import {Parallax} from 'react-parallax';
 
-const image1 =  "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+
+const image1 =  "https://cdn.britannica.com/06/96306-050-DFE8AFDC/Khao-Tapu-Ao-Phang-na-Thailand-National-Park.jpg"
 
 function HomePage() {
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
       <Parallax bgImage={image1} strength={500}>
         <div style={{height: 500}}>
-        <div className="text-in-box">HTML inside parallax</div>
+        <div className="text-in-box">JOIN NOMAD 
+        <h2>JOURNAL</h2></div>
         </div>
+        <p className="other-text">Document Your<br></br>Journey Today</p>
       </Parallax>
       <div style={{height: '100vh'}}></div>
+
+      <div className="home-container">
+        <img className="home-img" src="https://cdn.britannica.com/06/96306-050-DFE8AFDC/Khao-Tapu-Ao-Phang-na-Thailand-National-Park.jpg" alt="first-img">
+        </img>
+        <div className="home-text">
+          <h1>About Us</h1>
+        <h2>Cats like boxes because they are cryptic animals; they like to hide," Stephen Zawistowski, science adviser for the American Society for the Prevention of Cruelty to Animals, told Business Insider. "And a box gives them a place of safety and security."</h2>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/navabr.js
+++ src/components/navabr.js
@@ -1,22 +1,22 @@
-// import React from 'react'
-// import "./styles/nav.css";
+import React from 'react'
+import "./styles/nav.css";
 
 
-// export default function NavBar() {
-//     return (
-//         <div className="nav">
-//             <h1 className="logo-image">Nomad Diary</h1>
-//             <ul>
-//                 <li className="nav-list">
-//                    <a href="/" className='about'>About Us</a>
-//                    <a href="/" className='about'>Contact</a>
-//                    <a href="/" className='about'>Register</a>
-//                    <a href="/">
-//                        <span>Login</span>
-//                        <div className="liquid"></div>
-//                    </a>
-//                 </li>
-//             </ul>
-//         </div>
-//     )
-// }
+export default function NavBar() {
+    return (
+        <div className="nav">
+            <h1 className="logo-image">Nomad Diary</h1>
+            <ul>
+                <li className="nav-list">
+                   <a href="/" className='about'>About Us</a>
+                   <a href="/" className='about'>Contact</a>
+                   <a href="/" className='about'>Register</a>
+                   <a href="/">
+                       <span>Login</span>
+                       <div className="liquid"></div>
+                   </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
