f243083827e406d0d4d4851e3a60806cd05709af
blakelower <blakelower1999@gmail.com>
2020-05-09 17:05:14 -0700

    made a nav but its not done yet

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import PrivateRoute from "./components/privateRoute";
 import PostPage from "./components/postPage";
 import AddPost from "./components/addPost";
 import UpdatePost from "./components/updatePost";
+import NavBar from './components/navabr';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Switch>
         <Route exact path="/">
           <HomePage />
